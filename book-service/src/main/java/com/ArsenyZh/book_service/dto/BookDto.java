16295bbfd29b584f21b48038505557d3c0330872
Arseni Zhirnevich <szhirnevich@mail.ru>
2024-09-28 19:36:28 +0300

    create DTO for book object

---
+++ book-service/src/main/java/com/ArsenyZh/book_service/dto/BookDto.java
@@ -0,0 +1,12 @@
+package com.ArsenyZh.book_service.dto;
+
+import lombok.Data;
+
+@Data
+public class BookDto {
+    private String isbn;
+    private String name;
+    private String genre;
+    private String description;
+    private String author;
+}
