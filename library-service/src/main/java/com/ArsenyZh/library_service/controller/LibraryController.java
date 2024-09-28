300c292cff78b3f0a2b026853b8b01d41bb6f89f
Arseni Zhirnevich <szhirnevich@mail.ru>
2024-09-28 21:32:02 +0300

    create controller for library object

---
+++ library-service/src/main/java/com/ArsenyZh/library_service/controller/LibraryController.java
@@ -0,0 +1,46 @@
+package com.ArsenyZh.library_service.controller;
+
+import com.ArsenyZh.library_service.dto.LibraryDto;
+import com.ArsenyZh.library_service.mapper.LibraryMapper;
+import com.ArsenyZh.library_service.service.LibraryService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/api/v1/library")
+@RestController
+@AllArgsConstructor
+public class LibraryController {
+    private LibraryService libraryService;
+    private LibraryMapper libraryMapper;
+
+    @GetMapping("/")
+    public List<LibraryDto> getAllLibrary () {
+        List<LibraryDto> libraryDtoList = libraryMapper.convertLibraryListToLibraryDtoList(libraryService.findAllLibraries());
+
+        return libraryDtoList;
+    }
+
+    @GetMapping("/add_book")
+    public LibraryDto addBookToLibrary (Long bookId) {
+        LibraryDto libraryDto = libraryMapper.convertLibraryToLibraryDto(libraryService.saveBookToLibrary(bookId));
+        System.out.println(bookId);
+
+        return libraryDto;
+    }
+
+    @PostMapping("/take_book/{id}")
+    public LibraryDto takeBookFromLibrary (@PathVariable("id") Long id) {
+        LibraryDto libraryDto = libraryMapper.convertLibraryToLibraryDto(libraryService.takeBookFromLibrary(id));
+
+        return libraryDto;
+    }
+
+    @PostMapping("/return_book/{id}")
+    public LibraryDto returnBookToLibrary (@PathVariable("id") Long id) {
+        LibraryDto libraryDto = libraryMapper.convertLibraryToLibraryDto(libraryService.returnBookToLibrary(id));
+
+        return libraryDto;
+    }
+}
