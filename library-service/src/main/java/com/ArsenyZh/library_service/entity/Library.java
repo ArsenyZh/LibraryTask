e2792be07523346822021d654d6f3cc92f8887df
Arseni Zhirnevich <szhirnevich@mail.ru>
2024-09-28 20:48:08 +0300

    create entity for library object

---
+++ library-service/src/main/java/com/ArsenyZh/library_service/entity/Library.java
@@ -0,0 +1,24 @@
+package com.ArsenyZh.library_service.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Table(name = "library")
+public class Library {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "books_id")
+    private Long bookId;
+
+    @Column(name = "time_taken")
+    private LocalDate timeTaken;
+
+    @Column(name = "time_due")
+    private LocalDate timeDue;
+}
