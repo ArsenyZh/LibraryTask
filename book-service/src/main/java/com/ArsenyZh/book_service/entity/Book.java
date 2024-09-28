67f4378c7b615bd8a0a3dd472f97c4c38b1b411c
Arseni Zhirnevich <szhirnevich@mail.ru>
2024-09-28 19:34:04 +0300

    create entity for book object

---
+++ book-service/src/main/java/com/ArsenyZh/book_service/entity/Book.java
@@ -0,0 +1,28 @@
+package com.ArsenyZh.book_service.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "books")
+@Data
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "isbn")
+    private String isbn;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "genre")
+    private String genre;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "author")
+    private String author;
+}
