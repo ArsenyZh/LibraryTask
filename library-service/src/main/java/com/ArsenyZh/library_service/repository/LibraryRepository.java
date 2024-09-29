0ed1093822906e3bd24923dd148afcb60580bf6b
Arseni Zhirnevich <szhirnevich@mail.ru>
2024-09-30 00:17:03 +0300

    add query: find all free books in library

--- library-service/src/main/java/com/ArsenyZh/library_service/repository/LibraryRepository.java
+++ library-service/src/main/java/com/ArsenyZh/library_service/repository/LibraryRepository.java
@@ -2,9 +2,15 @@ package com.ArsenyZh.library_service.repository;
 
 import com.ArsenyZh.library_service.entity.Library;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface LibraryRepository extends JpaRepository<Library, Long> {
     Library findLibraryByBookId(Long bookId);
+
+    @Query("SELECT bookId FROM Library WHERE timeDue IS NULL")
+    List<Long> findAllByTimeDueIsNotNull();
 }
