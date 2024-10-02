0c435d7508c44f94971c01a92f33c84186eb89d2
Arseni Zhirnevich <szhirnevich@mail.ru>
2024-10-02 03:51:59 +0300

    enable connection to eureka service

--- book-service/src/main/java/com/ArsenyZh/book_service/controller/BookController.java
+++ book-service/src/main/java/com/ArsenyZh/book_service/controller/BookController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("/api/v1/books")
+//@RequestMapping("/api/v1/books")
 @RestController
 @AllArgsConstructor
 public class BookController {
