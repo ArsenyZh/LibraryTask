ed6c500423a7f1869352cf17daa4ff3244393738
Arseni Zhirnevich <szhirnevich@mail.ru>
2024-10-04 23:05:43 +0300

    fix feign client bug

--- book-service/src/main/java/com/ArsenyZh/book_service/feign/LibraryServiceFeignClient.java
+++ book-service/src/main/java/com/ArsenyZh/book_service/feign/LibraryServiceFeignClient.java
@@ -4,7 +4,7 @@ package com.ArsenyZh.book_service.feign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "library-service", url = "http://localhost:8082/api/v1/library/")
+@FeignClient(name = "library-service", url = "http://localhost:8082/")
 public interface LibraryServiceFeignClient {
 
     @PostMapping("/add_book")
